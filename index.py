f8a88e9ea0614914c49e790e8cd60b8154fd7c65
Anura <165815745+anura57@users.noreply.github.com>
2024-07-01 21:48:01 +0545

    Update index.py

--- index.py
+++ index.py
@@ -5,3 +5,11 @@
     #print("a is 1")
 #else:
     #print("This is error.")
+from tkinter import Tk, Entry, Button, StringVar
+
+class Calculator:
+    def __init__(self,master):
+        master.title("Calculator")
+    
+    root=Tk()
+    root.mainloop()
