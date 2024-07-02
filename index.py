9e19db3f3672ae9d903bb59ec60b89d285a2442d
Anura <165815745+anura57@users.noreply.github.com>
2024-07-02 13:19:18 +0545

    Update index.py

--- index.py
+++ index.py
@@ -5,4 +5,18 @@
     #print("a is 1")
 #else:
     #print("This is error.")
+from tkinter import Tk, Entry, Button, StringVar
 
+class Calculator:
+    def __init__(self,master):
+        master.title("Calculator")
+        master.geometry('37x420+0+0')
+        master.config(bg='gray')
+        master.resizable(False, False)
+
+        self.equation=StringVar()
+        self.entry_value=''
+        Entry(width=18,bg='#fff', font=('Poppins', 28))
+
+    root=Tk()
+    root.mainloop()
